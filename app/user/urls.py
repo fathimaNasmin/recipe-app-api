535cf74c2859f6371e984765ef68c6d6b685d3fa
Fathima nasmin <fathimanesmi@gmail.com>
2024-01-09 07:27:17 -0500

    created token api view

--- app/user/urls.py
+++ app/user/urls.py
@@ -8,5 +8,6 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    path('create/', views.CreateUserAPIView.as_view(), name='create')
+    path('create/', views.CreateUserAPIView.as_view(), name='create'),
+    path('token/', views.CreateTokenAPIView.as_view(), name='token'),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,10 +1,17 @@
 """Views for API"""
 
 from rest_framework import generics 
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserAPIView(generics.CreateAPIView):
     """API view class to create new user"""
     serializer_class = UserSerializer
+     
+class CreateTokenAPIView(ObtainAuthToken):
+    """API view class to create token for authenticated users."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
