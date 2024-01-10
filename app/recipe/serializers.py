033453437e5993293ed9f538b9c27b0c54c43dad
Fathima nasmin <fathimanesmi@gmail.com>
2024-01-10 11:27:26 -0500

    serializer for recipe

---
+++ app/recipe/serializers.py
@@ -0,0 +1,17 @@
+"""
+Serializers for Recipe api.
+"""
+
+from rest_framework import serializers 
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer class for recipe."""
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        readonly_fields = ['id']
+        
+        
