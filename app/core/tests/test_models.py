afa56464d9d1b7173f8b7ee449e831694df99f66
Fathima nasmin <fathimanesmi@gmail.com>
2024-01-12 05:03:15 -0500

    write test for tag model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,6 +8,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='test@example.com', password='password123'):
+    """Creates and returns user."""
+    return get_user_model().objects.create_user(email=email, password=password)
+
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -66,3 +71,8 @@ class ModelTests(TestCase):
         
         self.assertEqual(str(recipe), recipe.title)
         
+    def test_create_tag(self):
+        """Test creating tags."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+        self.assertEqual(str(tag), tag)
